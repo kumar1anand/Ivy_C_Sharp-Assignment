d7f0f6481e9ee1a56eff5eb9b66301b5a1b81f70
Anand Kumar <Anand.Kumar@ivycomptech.com>
2022-09-26 19:06:59 +0530

    Add project files.

---
+++ DA_Assignment_26_sept/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DA_Assignment_26_sept
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Declaraction
+            string str = @"C:\Users\anand.kumar\OneDrive - Entain Group\Documents\DA_Assignment\append_some_text.txt";
+            StreamWriter sw=File.AppendText(str);//open stream writer to append file.
+            //accept comment from user in a string variable
+            Console.WriteLine("Plese enter your text: ");
+            string str2=Console.ReadLine();
+            sw.WriteLine(str2);
+            sw.Close();
+        }
+    }
+}
